8297663e3cee5776f471ed8d52afaa45ea64f236
Ron Rayes <133511136+ronrayes2@users.noreply.github.com>
2023-05-20 16:20:57 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://animo1.000webhostapp.com` + encodeURI(path as string)
+					`https://animo1.000webhostapp.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
